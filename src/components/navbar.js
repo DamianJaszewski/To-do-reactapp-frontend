ff6fd7b8cf88c443ad0856ce124b2de3dc96face
Damian Jaszewski <damianjaszewski@gmail.com>
2022-02-11 12:46:47 +0100

    add routing via buttons

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,15 +1,11 @@
 import React from "react";
-import Axios from "axios"; 
+import {Link} from 'react-router-dom'
 
 // We import bootstrap to make our application look better.
 import "bootstrap/dist/css/bootstrap.css";
  
 // Here, we display our Navbar
 export default function Navbar() {
-
-  const getEvent = () =>{
-    Axios.get("http://localhost:3000/kalendarz")
-  }
   
  return (
   <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
@@ -17,11 +13,11 @@ export default function Navbar() {
   </a>
 
   <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-    <button type="submit" class="btn btn-primary" onClick={getEvent}>Połącz z kalendarzem</button>
+    <Link class='btn btn-primary' to='/'>Panel zadań</Link>
   </ul>
 
   <div class="col-md-3 text-end">
-    <button type="button" class="btn btn-outline-primary me-2">Login</button>
+    <Link class='btn btn-outline-primary me-2' to='login'>Login</Link>
     <button type="button" class="btn btn-primary">Log-out</button>
   </div>
 </header>
