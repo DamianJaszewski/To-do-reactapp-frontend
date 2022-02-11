b5f768d80d9e5834417732d335c01008347ca23f
Damian Jaszewski <damianjaszewski@gmail.com>
2022-02-11 11:17:39 +0100

    edit navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,41 +1,29 @@
 import React from "react";
- 
+import Axios from "axios"; 
+
 // We import bootstrap to make our application look better.
 import "bootstrap/dist/css/bootstrap.css";
  
-// We import NavLink to utilize the react router.
-import { NavLink } from "react-router-dom";
- 
 // Here, we display our Navbar
 export default function Navbar() {
+
+  const getEvent = () =>{
+    Axios.get("http://localhost:3000/kalendarz")
+  }
+  
  return (
-   <div>
-     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-       <NavLink className="navbar-brand" to="/">
-       <img style={{"width" : 25 + '%'}} src="https://d3cy9zhslanhfa.cloudfront.net/media/3800C044-6298-4575-A05D5C6B7623EE37/4B45D0EC-3482-4759-82DA37D8EA07D229/webimage-8A27671A-8A53-45DC-89D7BF8537F15A0D.png"></img>
-       </NavLink>
-       <button
-         className="navbar-toggler"
-         type="button"
-         data-toggle="collapse"
-         data-target="#navbarSupportedContent"
-         aria-controls="navbarSupportedContent"
-         aria-expanded="false"
-         aria-label="Toggle navigation"
-       >
-         <span className="navbar-toggler-icon"></span>
-       </button>
- 
-       <div className="collapse navbar-collapse" id="navbarSupportedContent">
-         <ul className="navbar-nav ml-auto">
-           <li className="nav-item">
-             <NavLink className="nav-link" to="/create">
-               Create Record
-             </NavLink>
-           </li>
-         </ul>
-       </div>
-     </nav>
-   </div>
+  <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+  <a href="https://getbootstrap.com/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+  </a>
+
+  <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+    <button type="submit" class="btn btn-primary" onClick={getEvent}>Połącz z kalendarzem</button>
+  </ul>
+
+  <div class="col-md-3 text-end">
+    <button type="button" class="btn btn-outline-primary me-2">Login</button>
+    <button type="button" class="btn btn-primary">Sign-up</button>
+  </div>
+</header>
  );
 }
